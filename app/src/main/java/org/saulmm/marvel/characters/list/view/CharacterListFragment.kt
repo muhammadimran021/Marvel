fd1ae42c33a1fd2e7a7505855b2931ba8b6a70fe
saulmm <saulmm2@gmail.com>
2021-12-26 16:37:32 +0100

    Created CharacterListViewModel.kt

--- app/src/main/java/org/saulmm/marvel/characters/list/view/CharacterListFragment.kt
+++ app/src/main/java/org/saulmm/marvel/characters/list/view/CharacterListFragment.kt
@@ -1,6 +1,11 @@
 package org.saulmm.marvel.characters.list.view
 
+import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import dagger.hilt.android.AndroidEntryPoint
 import org.saulmm.marvel.R
 import org.saulmm.marvel.databinding.FragmentCharacterListBinding
 import org.saulmm.marvel.utils.ext.viewBinding
@@ -13,4 +18,10 @@ class CharacterListFragment: Fragment(R.layout.fragment_character_list) {
     }
 
     private val binding by viewBinding(FragmentCharacterListBinding::bind)
+    private val viewModel: CharacterListViewModel by viewModels()
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        Toast.makeText(requireContext(), "Viewmodel here: $viewModel", Toast.LENGTH_SHORT).show()
+    }
 }
